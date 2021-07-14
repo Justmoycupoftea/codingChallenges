44377e6dedb39fc47241403583c8a5c73cc4558e
Justin Moy <moy.justin94@gmail.com>
2021-07-14 15:59:44 -0500

    updating old codewar solns

---
+++ oldCodeWars/main.js
@@ -0,0 +1,10 @@
+//attempted codewar ladder 8kyu - 7kyu - 6kyu - 7kyu -8kyu
+
+// Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accept 1 parameters:n, 
+// n is the number of customers to buy hotdogs, different numbers have different 
+// prices (refer to the following table), return a number that the customer need to pay how much money.
+
+function saleHotdogs(n){
+    if (n < 5) return n * 100;
+    if (n >= 5 && n < 10) return n * 95; else return n * 90;
+  }
