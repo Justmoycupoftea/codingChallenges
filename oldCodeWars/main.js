2ce93275a7e2a9ab8a444af0d585b5f6f671876a
Justin Moy <moy.justin94@gmail.com>
2021-07-20 19:23:40 -0500

    updating codewar problem

--- oldCodeWars/main.js
+++ oldCodeWars/main.js
@@ -44,4 +44,25 @@ return str.map(function(el, i){
 function toCamelCase(str){
     return str.replace(/[-_](.)/g, (_, c) => c.toUpperCase());
   }
+
+//   Given a list of integers, determine whether the sum of its elements is odd or even.
+
+//   Give your answer as a string matching "odd" or "even".
   
+//   If the input array is empty consider it as: [0] (array with a zero).
+
+function oddOrEven(array) {
+    let sum = 0;
+    
+    if (array.length !== 0) {
+      sum = array.reduce((x, y) => x + y);
+      return sum % 2 === 0 ? 'even' : 'odd';
+      
+    } else {
+      return 'even';
+    }
+}
+//other soln
+function oddOrEven(arr) {
+    return arr.reduce((a,b)=>a+b,0) % 2 ? 'odd' : 'even';
+  }
