d8e2131a41e06531f7ec5e1c328bdbe438105ff0
Justin Moy <moy.justin94@gmail.com>
2022-03-27 15:53:40 -0500

    updating codewar folder with comments on old solns

--- CC_5-9-22/main.js
+++ CC_5-9-22/main.js
@@ -21,3 +21,22 @@ function countPositivesSumNegatives(input) {
 return[sum2, sum1]
 
 }
+
+//correct soln
+function countPositivesSumNegatives(input) {
+    if (input == null || input.length == 0)
+      return [];
+    
+    var positive = 0;
+    var negative = 0;
+    
+    for (var i=0, l=input.length; i<l; ++i)
+    {
+      if (input[i] > 0)
+        ++ positive;
+      else
+        negative += input[i];
+    }
+    
+    return [positive, negative];
+}
