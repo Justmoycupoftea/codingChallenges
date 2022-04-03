603f7bf25ad04a958ae7f622130084bbb4b75318
Justin Moy <moy.justin94@gmail.com>
2022-04-02 19:30:52 -0500

    updating codewar folder with comments on old solns

--- CC_5-8-22/main.js
+++ CC_5-8-22/main.js
@@ -1,5 +1,7 @@
 // Complete the method that takes a boolean value and 
 // return a "Yes" string for true, or a "No" string for false.
 
+//spent 30mins looking for more complex answer
+return bool ? 'Yes':'No'
 
-
+//simple soln
