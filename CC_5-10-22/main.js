d2139355b453a96799428fe943080f63b96e0431
Justin Moy <moy.justin94@gmail.com>
2022-05-10 14:39:36 -0500

    updating codewars 5-10-22

--- CC_5-10-22/main.js
+++ CC_5-10-22/main.js
@@ -31,4 +31,30 @@ function setAlarm(employed, vacation){
 //simple soln
 function setAlarm(employed, vacation){
     return (employed && !vacation) ? true : false;
-  }
+}
+
+//anotha one since i did bad
+
+// Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+// Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+// Examples:
+
+// Testing: [0, 0, 0, 1] ==> 1
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 0, 1] ==> 5
+// Testing: [1, 0, 0, 1] ==> 9
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 1, 0] ==> 6
+// Testing: [1, 1, 1, 1] ==> 15
+// Testing: [1, 0, 1, 1] ==> 11
+
+const binaryArrayToNumber = arr => parseInt(arr.join(''), 2); //It's the base of the number we will parse: 2, instead of the default 10.
+//join() join up the array elements in a string, and parseInt() convert the array in decimal,
+// the second value 2 in the method represent the decimal number system
+// The join('') method merges all the array elements into a string. 
+// The parseInt(num, base) convert the 'num' represented in base 'base' into a decimal base. For example
+
+// if arr = [1, 0, 1] then arr.join('-') --> '1-0-1', arr.join('X') --> '1X0X1', arr.join('') --> '101'
+// parseInt('1ea', 16) --> 490, parseInt('11', 8) --> 9, parseInt('101', 2) --> 5
