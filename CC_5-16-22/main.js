4459444ded2a5f5dabc644e6585a67dd16692743
Justin Moy <moy.justin94@gmail.com>
2022-05-16 17:36:40 -0500

    codewar problem and soln for 5-16-22

---
+++ CC_5-16-22/main.js
@@ -0,0 +1,23 @@
+// You will be given an array a and a value x. All you need to do 
+
+// is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+//PREP
+//Parameters
+array = a, value = x
+//Returns
+checking if array, a contains the value, x
+//Examples
+if(x ) //didn't need to use condtional if x == to string || num
+return a.includes(x) ? true : false
+//Psuedo code
+
+function check(a, x) {
+    // your code here
+    return a.includes(x) ? true : false
+  
+}
