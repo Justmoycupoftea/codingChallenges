400ce2217c02b305a4e0302b137f6e3f7db20df6
Justin Moy <moy.justin94@gmail.com>
2022-05-18 16:21:50 -0500

    adding forgotten things

---
+++ arrayLadders678/main.js
@@ -0,0 +1,88 @@
+// Given an array of integers your solution should find the smallest integer.
+
+// For example:
+
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+//PREP
+//8 kyu
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      return Math.min(...args) //spread operator spreads the entire array, math min will find the lowest
+    }
+  }
+
+//find smaller values, use sort
+//return the smallest value
+//example sort[0], Math.min findest the lowest number, spread operator spreads it all out
+
+//7kyu
+function sortByLength (array) {
+    // Return an array containing the same strings, ordered from shortest to longest
+    return array.sort((a,b) => a.length - b.length)
+
+  };
+
+//use sort()
+
+//6kyu
+// You are going to be given an array of integers. Your job is to take that array and find an index N where the sum of the integers to the left of N is equal to the sum of the integers to the right of N. If there is no index that would make this happen, return -1.
+
+// For example:
+
+// Let's say you are given the array {1,2,3,4,3,2,1}:
+// Your function will return the index 3, because at the 3rd position of the array, the sum of left side of the index ({1,2,3}) and the sum of the right side of the index ({3,2,1}) both equal 6.
+
+// Let's look at another one.
+// You are given the array {1,100,50,-51,1,1}:
+// Your function will return the index 1, because at the 1st position of the array, the sum of left side of the index ({1}) and the sum of the right side of the index ({50,-51,1,1}) both equal 1.
+
+// Last one:
+// You are given the array {20,10,-80,10,10,15,35}
+// At index 0 the left side is {}
+// The right side is {10,-80,10,10,15,35}
+// They both are equal to 0 when added. (Empty arrays are equal to 0 in this problem)
+// Index 0 is the place where the left side and right side are equal.
+
+// Note: Please remember that in most programming/scripting languages the index of an array starts at 0.
+
+// Input:
+// An integer array of length 0 < arr < 1000. The numbers in the array can be any integer positive or negative.
+
+// Output:
+// The lowest index N where the side to the left of N is equal to the side to the right of N. If you do not find an index that fits these rules, then you will return -1.
+
+// Note:
+// If you are given an array with multiple answers, return the lowest correct index.
+
+function findEvenIndex(arr)
+{   let equalIndex = -1
+  //Code goes here!
+  arr.forEach((item,index) => {
+      let leftSide = arr.slice(0,index).reduce((sum,item) => sum + item,0)
+      let rightSide = arr.slice(index + 1).reduce((sum,item) => sum + item,0)
+      if (leftSide == rightSide)
+          {equalIndex = index
+        }
+          console.log(index)
+
+
+      
+  });
+  return equalIndex
+}
+
+function findEvenIndex(arr)
+{
+  var left = 0, right = arr.reduce(function(pv, cv) { return pv + cv; }, 0);
+  for(var i = 0; i < arr.length; i++) {
+      if(i > 0) left += arr[i-1];
+      right -= arr[i];
+      
+      if(left == right) return i;
+  }
+  
+  return -1;
+}
