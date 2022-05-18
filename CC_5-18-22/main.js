68ad843a6856ea3570787cac81b0d3e46bc17486
Justin Moy <moy.justin94@gmail.com>
2022-05-18 16:21:24 -0500

    codewar problem and soln 5-18-22

---
+++ CC_5-18-22/main.js
@@ -0,0 +1,17 @@
+// Make a function that will return a greeting statement that uses an input; 
+
+// your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+//PREP
+//P:
+//a parameter, name will be passed, generate a statement with using the namne
+//R: returning parameter with 'name'
+//E: name = justy,  "Hello, justy how are you doing today?"
+//P: temporal literal `` ${name} place into string 'hello ${name}'
+
+function greet(name){
+    //your code here
+    return `Hello, ${name} how are you doing today?`
+  }
