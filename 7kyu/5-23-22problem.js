f1c59b8ceddbc175e3a81e5b51f274ce42aafb35
Justin Moy <moy.justin94@gmail.com>
2022-05-23 18:23:14 -0500

    codewar problem and soln updates for 5-23-22

--- 7kyu/5-23-22problem.js
+++ 7kyu/5-23-22problem.js
@@ -62,3 +62,24 @@ function calculateYears(principal, interest, tax, desired) {
     }
     return years;
 }
+
+//another prob wasn't satisfied with first
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+// accepts 3 integers for values a, b, c they all have to be greater than 0? 
+//P: what is taken in, will it always be a number? no string or boolean? always whole? 
+//R: return number true if numbers are > 0 
+//E:
+function isTriangle(a,b,c){
+    if(a + b > c) && (b + c > a) && (c + a > b){
+        return true
+    }else{
+        false
+    }
+}
+//triangle formula
+function isTriangle(a,b,c)
+{
+   return a + b > c && a + c > b && c + b > a;
+}
