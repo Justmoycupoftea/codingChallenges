3f8ed5001211c47546db97e314bac24f5f96e997
Justin Moy <moy.justin94@gmail.com>
2022-06-02 20:43:58 -0500

    updated codewar problem and soln for 6-2-22

--- 7kyu/6-2-22problem.js
+++ 7kyu/6-2-22problem.js
@@ -10,3 +10,20 @@
 // number(["a", "b", "c"]) // => ["1: a", "2: b", "3: c"]
 //code war problem 
 //PREP
+
+var number = function(array) {
+    return array.map(function (line, index) {
+      return (index + 1) + ": " + line;
+    });
+}
+
+// let map = function(array,func) { //declare array variable where we will store each functions operation on each element let arrayToReturn = [] //iterate the array on which we will call the function for(let i = 0; i < array.length; i++ ) { //in the function we pass in we have the ability to do what ever we want to that element
+//     // val index, array
+//     arrayToReturn.push(func(array[i], i, array)) //once the operation is complete you return that value which then gets pushed in to the above array } //now when the iteration is done on the whole array the last thing we need to do is return the completed array //this is pretty much how map works hope it helps you understand it a bit more!
+    
+//     } //id call this like so map(['1','2','3','4'], function(val, index) { return val + index }) or
+    
+//     map(['1','2','3','4'], (val, index) => val + index) //ull get something like this =) => [1, 3, 5, 7]
+
+//another soln
+let number = (a) => a.map((v, i) => `${i + 1}: ${v}`)
