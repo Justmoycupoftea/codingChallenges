686305e87d91b3bdad3d037ab0a99bcf3b121804
Justin Moy <moy.justin94@gmail.com>
2022-07-17 21:02:11 -0500

    codewar problem and soln 7-17-22

--- 7kyu/7-16-22problem.js
+++ 7kyu/7-16-22problem.js
@@ -22,11 +22,16 @@
 // maxMultiple (37,200) ==> return (185)
 // Explanation:
 // (185) is divisible by (37) , (185) is less than or equal to bound (200) , and (185) is > 0 .
+//37 goes into 200 5 times = 37*5 = 185
 
 //No real steps to this one. You just have to realize that bound % divisor = remainder and subtracting the remainder from bound will give you the highest integer multiple.
 
 //Example (2,7) -> 7%2 = 1; 7/2 = 3.5 in other words 7 contains whole number 2 three times which is 6 in total. Does it make sense?
 //by definition the remainder of a division is the amount you would need to substract from the bound in order to get a whole multiple
+
+//another exp
+//17%7=3 17-3 = 14, ans is 14
+//7 goes into 17 2 times which is 14 and 14 is the highest integer
 
 function maxMultiple(divisor, bound){
     return bound-bound%divisor
