18c634796db3e9142ee18194bfc11f654e1a54db
Justin Moy <moy.justin94@gmail.com>
2022-08-16 21:01:27 -0500

    codewar soln update

--- 7kyu/alphabet-war.js
+++ 7kyu/alphabet-war.js
@@ -29,3 +29,36 @@ function alphabetWar(fight) {
     let result = fight.split('').reduce((a, b) => a + (map[b] || 0), 0);
     return result ? (result < 0 ? "Left" : "Right") + " side wins!" : "Let's fight again!";
 }
+
+//another soln
+
+function alphabetWar(fight) {
+    let left = 0;
+    let right = 0;
+
+    for (i = 0; i < fight.length; i++) {
+        (fight[i] === 'w')
+        ? left += 4
+        : (fight[i] === 'p')
+        ? left += 3
+        : (fight[i] === 'b')
+        ? left += 2
+        : (fight[i] === 's')
+        ? left += 1
+        : (fight[i] === 'm')
+        ? right += 4
+        : (fight[i] === 'q')
+        ? right += 3
+        : (fight[i] === 'd')
+        ? right += 2
+        : (fight[i] === 'z')
+        ? right += 1
+        : left += 0;
+    }
+    return (left > right) 
+      ? `Left side wins!`
+      : (left < right)
+      ? `Right side wins!`
+      : `Let's fight again!`
+
+}
