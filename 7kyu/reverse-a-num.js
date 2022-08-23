8b4d54e3b35daf8c7230ef9ac947b05cdd6f6c4e
Justin Moy <moy.justin94@gmail.com>
2022-08-23 15:06:30 -0500

    codewar problem and soln 8-23-22

--- 7kyu/reverse-a-num.js
+++ 7kyu/reverse-a-num.js
@@ -6,3 +6,15 @@
 //  123 ->  321
 // -456 -> -654
 // 1000 ->    1
+//P: will there be decimal nums?
+//R: return the string of num reversed with negative signs too and nums with 0s so 001 = 1
+//E: 123 -> 321
+//P: split the string, reverse, combine
+function reverseNumber(n) {
+    let isNegative = n < 0;  
+    let reverseAsString = Math.abs(n).toString().split('').reverse().join('');
+    let result = Number(reverseAsString);
+    
+    return isNegative ? -result : result;
+  }
+  
