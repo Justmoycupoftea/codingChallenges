4bccb6352b997cc48f7c42b984d80cb71ea55781
Justin Moy <moy.justin94@gmail.com>
2022-08-24 15:21:08 -0500

    codewar problem 8-24-22

---
+++ 7kyu/most-digits.js
@@ -0,0 +1,10 @@
+// Find the number with the most digits.
+
+// If two numbers in the argument array have the same number of digits, return the first one in the array.
+//P: will there be any neg nums?
+//R: return num with the most digits, but if the nums are the same return the first one
+//E: [2,1] -> [2]
+//P: sort the array and return the first one, but if the nums are the same length return the first one instead like in example
+function findLongest(array){
+
+  }
