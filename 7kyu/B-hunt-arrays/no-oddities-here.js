0832a43a54f17d7ee097aff38672722f63af9704
Justin Moy <moy.justin94@gmail.com>
2022-09-07 20:00:15 -0500

    added prep

--- 7kyu/B-hunt-arrays/no-oddities-here.js
+++ 7kyu/B-hunt-arrays/no-oddities-here.js
@@ -1,6 +1,10 @@
 // Write a small function that returns the values of an array that are not odd.
 
 // All values in the array will be integers. Return the good values in the order they are given.
+//P:
+//R:
+//E:
+//P:
 
 function noOdds( values ){
     // Return all non-odd values
