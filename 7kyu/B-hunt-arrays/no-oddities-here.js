2066aa1a2de2514820cbe87f4a4277bf32f27edf
Justin Moy <moy.justin94@gmail.com>
2022-09-09 20:18:13 -0500

    codewar soln

--- 7kyu/B-hunt-arrays/no-oddities-here.js
+++ 7kyu/B-hunt-arrays/no-oddities-here.js
@@ -8,4 +8,11 @@
 
 function noOdds( values ){
     // Return all non-odd values
+    let arr =[]
+    for(let i = 0; i < values.length; i++){
+        if (value[i] % 2 == 0){
+            arr.push(values[i])
+        }
+    }
+    return arr
   }
