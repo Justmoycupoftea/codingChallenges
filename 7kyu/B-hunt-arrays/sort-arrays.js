6c87346035128ee73ad89f0af96f69fbb5bbdb3d
Justin Moy <moy.justin94@gmail.com>
2022-09-12 15:26:24 -0500

    codewar problem

---
+++ 7kyu/B-hunt-arrays/sort-arrays.js
@@ -0,0 +1,11 @@
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+// input: names - unsorted array
+// output: sorted array
+//P: strings are coming in, will there be capitol letters? will there be nums? any special characters?
+//R: return elements being sorted in lexoigraphical order
+//E: (['one', 'two', 'three' ]), ["one", "three", "two"])
+//P: use sort method for lexicographical order
+sortme = function( names ){
+ return names.sort()
+}
