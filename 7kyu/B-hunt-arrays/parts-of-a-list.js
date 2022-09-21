f01864874c91de7a4a90d216c836a052c4fd8cbb
Justin Moy <moy.justin94@gmail.com>
2022-09-20 19:35:08 -0500

    updated soln

--- 7kyu/B-hunt-arrays/parts-of-a-list.js
+++ 7kyu/B-hunt-arrays/parts-of-a-list.js
@@ -24,3 +24,15 @@ function partlist(arr) {
 }
 
 //another soln
+function partlist(arr) {
+    var newArray;
+    var returnArray=[];
+    for( var i=1; i<arr.length; i++){
+      newArray = [];
+      newArray.push(arr.slice(0,i).join(" "));
+      newArray.push(arr.slice(i).join(" "));
+      returnArray.push(newArray);
+    }
+    
+    return returnArray;
+}
