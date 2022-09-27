4a46c59c75eb9da12fbd345655d0f0a69c00f98e
Justin Moy <moy.justin94@gmail.com>
2022-09-26 20:47:11 -0500

    codewar soln

--- 7kyu/B-hunt-arrays/even-nums-in-array.js
+++ 7kyu/B-hunt-arrays/even-nums-in-array.js
@@ -6,9 +6,24 @@
 // ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
 // ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
 //P:an array of numbers coming in, will there be decimals?
-//R:return even numbers from the array and return them in order
+//R:return even numbers from the array and return them in order and how many even numbers that are present in the array
 //E:
 // ([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
 // ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
 // ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
 //P: use a filter function and filter and return all even nums from array
+
+function evenNumbers(array, number) {
+    var EvenNumbers= [];
+    
+    for(var i=0; i<=array.length; i++)
+     {
+         if(array[i] % 2 ==0)
+           EvenNumbers.push(array[i]);
+     }
+    
+      return EvenNumbers.splice(EvenNumbers.length-number,number);
+      
+   }
+//another soln
+const evenNumbers = (array, number) => array.filter(item => item % 2 === 0).slice(-number);
