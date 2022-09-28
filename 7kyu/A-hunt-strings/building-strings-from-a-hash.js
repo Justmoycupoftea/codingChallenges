efd5a5df5e1fa7083221fb34f7a511b198245fa1
Justin Moy <moy.justin94@gmail.com>
2022-09-28 16:32:27 -0500

    codewar problem

--- 7kyu/A-hunt-strings/building-strings-from-a-hash.js
+++ 7kyu/A-hunt-strings/building-strings-from-a-hash.js
@@ -23,3 +23,9 @@ function solution(pairs){
     .map(function(k) { return k + ' = ' + pairs[k] })
     .join(',');
 }
+function solution(pairs){
+    return Object.keys(pairs) //returning Objects and properties inside
+    .map(function(k){return k + '=' + pairs[k]}) //mapping would copy the array and pass in the new element we pass in which is 'k'
+    //'k' would be the object and pairs[k] would be the value inside the object
+    .join(',') //variables are joined forming ({a: 1, b: '2'}) // should return "a = 1,b = 2"
+}
