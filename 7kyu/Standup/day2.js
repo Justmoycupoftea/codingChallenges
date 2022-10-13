9a4e197c5d0f194ffe73c13b22bd6a8dc9508cbf
Justin Moy <moy.justin94@gmail.com>
2022-10-13 17:23:42 -0500

    codewar problem 10/13/22

---
+++ 7kyu/Standup/day2.js
@@ -0,0 +1,27 @@
+// // Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+// // * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+// // * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+// // * url = "https://www.cnet.com"                -> domain name = cnet"
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+// domainName("http://github.com/carbonfive/raygun") == "github" 
+// domainName("http://www.zombie-bites.com") == "zombie-bites"
+// domainName("https://www.cnet.com") == "cnet
+
+//url trad no funny biz
+//str
+
+function parseDomainName(url){
+    return url.replace('https://','').replace('http://','').replace('www','').split('.')[0]
+
+    //replace the stuff upfront
+
+    //split grabbing the first value and return the name of website
+
+}
+
+console.log(parseDomainName('http://github.com/carbonfive/raygun'), 'github')
+console.log(parseDomainName('http://www.zombie-bites.com'), 'zombie-bites')
+console.log(parseDomainName('https://www.cnet.com'), 'cnet')
+
