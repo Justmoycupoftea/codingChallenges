3c29ef76395e3961e913f13224c50e7a27189257
Justin Moy <moy.justin94@gmail.com>
2022-10-13 13:04:40 -0500

    codewar problem

--- 7kyu/A-hunt-strings/building-strings-from-a-hash.js
+++ 7kyu/A-hunt-strings/building-strings-from-a-hash.js
@@ -38,21 +38,34 @@ function solution(pairs) {
 
 function solution(pairs){
   return Object.keys(pairs)
-  .map(function(k){return k + '=' + pairs[k]})
+  .map(function(k){return k + ' = ' + pairs[k]})
   .join(',')
 }
 
 function solution(pairs){
   return Object.keys(pairs) //return object and properties inside
-  .map(function(k){return k + '=' + pairs[k]}) //maping so copying array and changing it to parameter returned + '=' and pairs[k] would be value inside object
+  .map(function(k){return k + ' = ' + pairs[k]}) //maping so copying array and changing it to parameter returned + '=' and pairs[k] would be value inside object
   //finally join
   .join(',')
 }
 
 function solution(pairs){
   return Object.keys(pairs)
-  .map(function(k){return k + '=' + pairs[k]})
+  .map(function(k){return k + ' = ' + pairs[k]})
   //copying array from object keys which would be a or b and the pairs[k] accessing key in object
   //which would be the num so it would be a = 1 b = 2 and join with commas
+  //spacing between = is important bc problem wants the space
   .join(',')
+}
+
+//taking object and generating human readable string from its key/value pairs
+
+//will there be any negative nums? special letters? decimals?
+//returning something human readable? such as string from its key/value pairs
+//solution({a: 1, b: '2'}) // should return "a = 1,b = 2"
+//return object.keys(pairs) use a map to generate array with function(k){return k + '=' pairs[k]}
+//join with a comma to equal out a = 1, b = 2
+
+function solution(pairs){
+  // TODO: complete
 }
