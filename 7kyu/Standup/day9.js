3b9ad3a451f2cf507d2dfc48f478e7939b6a0606
Justin Moy <moy.justin94@gmail.com>
2022-10-27 17:27:33 -0500

    codewar 10-27-22

---
+++ 7kyu/Standup/day9.js
@@ -0,0 +1,27 @@
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+// Examples
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))((" 
+// Notes
+// Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+
+//UTF characters, any empty spaces allowed or more than one of the same character allowed, will there be cases where it's just 15 characters?
+//return a string made up of left and right parenthesis 
+
+function duplicateEncode(word){
+    //toLowerCase
+    //turn string into array have to split then use map split -> map -> indexOf === lastIndexOf ? '(' : ')'
+    //join
+    return word.toLowerCase()
+                .split()
+                .map((l, i, a) => a.indexOf(l) === a.lastIndexOf(l) ) ? '(' : ')'
+                //current letter equal to last letter
+                .join('')
+}
+
+console.log(duplicateEncode('din'), '(((')
+console.log(duplicateEncode('recede'), '()()()')
+
