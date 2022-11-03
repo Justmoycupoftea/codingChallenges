9055066d0b9771404993f0076d90d89f721553b4
Justin Moy <moy.justin94@gmail.com>
2022-11-03 17:18:06 -0500

    codewar problem 11/3/22

--- 7kyu/Standup/day11.js
+++ 7kyu/Standup/day11.js
@@ -4,6 +4,18 @@
 
 // All inputs will be valid.
 
-function last(x){
+//will it alays be words, will it be empty, any spaces or special characters
+//return sorted array of words and the last letter of each word
 
+function last(x){
+    //split sort
+    //sort -> charAt()
+    //grabs the word and gets last letter with .length - 1
+    return x.split(' ').sort((a,b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1))
 }
+
+//console log test cases
+console.log(sortLastChar())
+
+//doing in ES6 one line
+const last = ( l ) => l.split(' ').sort((a,b)=> a.slice(-1) > b.slice(-1))
