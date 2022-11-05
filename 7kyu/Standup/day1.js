710b2e9cbcd3356eb5bf2916857eb6623206554a
Justin Moy <moy.justin94@gmail.com>
2022-11-05 14:53:37 -0500

    codewar soln update

--- 7kyu/Standup/day1.js
+++ 7kyu/Standup/day1.js
@@ -96,3 +96,17 @@ function fizzBuzz(nums){
         }
     }
 }
+
+function fizzbuzz(nums){
+    for (let i = 1; i <= num; i++){
+        if(i % 5 === 0 && i % 3 === 0){
+            return 'fizzbuzz'
+        }else if(i % 5 === 0){
+            return 'fizz'
+        }else if(i % 3 === 0){
+            return 'buzz'
+        }else{
+            return i
+        }
+    }
+}
