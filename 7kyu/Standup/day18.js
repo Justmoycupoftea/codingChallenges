d4e13bae1e297fabb48b86489e181c6c5ec6e4c0
Justin Moy <moy.justin94@gmail.com>
2022-11-28 15:30:31 -0600

    codewar problem 11/28/22

---
+++ 7kyu/Standup/day18.js
@@ -0,0 +1,16 @@
+// In this Kata, you will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+
+// For example:
+
+// dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+
+// dup(["kelless","keenness"]) = ["keles","kenes"].
+
+// ["ccooddddddewwwaaaaarrrrsssss","piccaninny","hubbubbubboo"],['codewars','picaniny','hubububo'];
+// ["abracadabra","allottee","assessee"],['abracadabra','alote','asese'];
+// ["kelless","keenness"], ['keles','kenes'];
+// ["Woolloomooloo","flooddoorroommoonlighters","chuchchi"], ['Wolomolo','flodoromonlighters','chuchchi'];
+
+function dup(s) {
+    //..
+    };
