9b1ad8c38eaa052f5e5adaa457f4c3d744be5afe
Justin Moy <moy.justin94@gmail.com>
2022-12-05 17:01:23 -0600

    codewar problem soln

--- 7kyu/Standup/day22.js
+++ 7kyu/Standup/day22.js
@@ -1,3 +1,22 @@
 // Count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
 
 // What if the string is empty? Then the result should be empty object literal, {}.
+
+function solution(pairs){
+    return Object.keys(pairs)
+      .map(function(k) { return k + ' = ' + pairs[k] })
+      .join(',');
+  }
+
+  function solution(pairs){
+    return Object.keys(pairs)
+    .map(function(k) { return k + '=' + pairs[k] })
+    .join(',');
+  }
+
+//other soln
+function solution(pairs) {
+    return Object.keys(pairs).map(k => `${k} = ${pairs[k]}`).join(",");
+  }
+
+//leon's soln
