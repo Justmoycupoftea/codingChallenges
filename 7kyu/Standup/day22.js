811b2c161790421d11ce0c60a4d5853dc13e3fa1
Justin Moy <moy.justin94@gmail.com>
2022-12-06 17:26:17 -0600

    leetcode prob 12/06/22

--- 7kyu/Standup/day22.js
+++ 7kyu/Standup/day22.js
@@ -17,6 +17,26 @@ function solution(pairs){
 //other soln
 function solution(pairs) {
     return Object.keys(pairs).map(k => `${k} = ${pairs[k]}`).join(",");
-  }
+}
 
+//my soln solved a diff problem
 //leon's soln
+function countChars(str){
+    //create obj
+    let countObj = {}
+    //loop -> check val exist ? val++ : val-1
+    for(const c of str){
+        //loop moves through each char in string
+        if( countObj[c] ){
+            countObj[c]++
+        }else{
+            countObj[c] = 1
+        }
+    }
+    return countObj
+
+}
+
+console.log(countChars('aba'), "{'a': 2, 'b': 1}")
+console.log(countChars('dog'), "{'d': 1, 'o': 1, 'g':1}")
+console.log(countChars('abba'), "{'a': 2, 'b': 2}")
