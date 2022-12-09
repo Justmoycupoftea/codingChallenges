0a9a01d3090b97c8004339f4f816fd0f74986a96
Justin Moy <moy.justin94@gmail.com>
2022-12-08 21:33:30 -0600

    leetcode 12/08/22

--- 7kyu/leetcode/day2.js
+++ 7kyu/leetcode/day2.js
@@ -7,3 +7,25 @@
 // });
 
 // No String or Array Methods (well brute force it first, but then no methods)! 
+
+function maxCharacter(str){
+    let charMap = {},
+    count = 0,
+    maxChar = null
+
+    //create object
+    for(const char of str){
+        charMap[char] = charMap[char] + 1 || 1
+        
+    }
+    //looping through
+    for(const char in charMap){
+        if(charMap[char] > count){
+            count = charMap[char]
+            maxChar = char
+        }
+    }
+    return maxChar
+}
+
+console.log(maxCharacter("Hello World!"), 'l')
