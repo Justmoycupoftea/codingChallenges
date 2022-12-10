95f34532454d1012e8a8eef775e2b43175c078c7
Justin Moy <moy.justin94@gmail.com>
2022-12-10 11:16:02 -0600

    worked on codewar solns

--- 7kyu/Standup/day2.js
+++ 7kyu/Standup/day2.js
@@ -49,3 +49,10 @@ function domainName(url){
                .replace('www.', '')
                .split('.')[0];
   }
+
+  function domainName(url){
+    return  url.replace('http://', '')
+               .replace('https://', '')
+               .replace('www.', '')
+               .split('.')[0];
+  }
