3d25fdb3582e13f2d9eca74e9fc1afad6fffa09e
Justin Moy <moy.justin94@gmail.com>
2022-12-14 18:35:13 -0600

    leetcode 12/14/22

--- 7kyu/leetcode/day4.js
+++ 7kyu/leetcode/day4.js
@@ -11,3 +11,23 @@
 // Input: nums = [2,2,1,1,1,2,2]
 // Output: 2
 
+function majorityElement(arr){
+    let elem = {},
+        count = 0,
+        majElem = [0]
+    
+    for(const num of arr){
+            elem[num] = elem[num] + 1 || 1
+            //can use shorter way
+            //elem[num] > arr.length / 2
+    }
+
+    for(const n in elem){
+        if(elem[n] > count){
+            count = elem[n]
+            majElem = n
+        }
+    }
+
+    return majElem
+}
