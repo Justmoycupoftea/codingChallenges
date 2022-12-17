4ec0ed0cc5f38382fef12eac1363cf0f416f78be
Justin Moy <moy.justin94@gmail.com>
2022-12-16 21:05:03 -0600

    leetcode soln breakdown

--- 7kyu/leetcode/day6.js
+++ 7kyu/leetcode/day6.js
@@ -23,3 +23,15 @@ function twoSum(arr, sum){
 }
 
 console.log(twoSum([1, 2, 2, 3, 4], 4),'[[2, 2], [3, 1]]')
+
+function twoSums(arr, sum){
+    const pairs = []
+    const nums = {}
+
+    for(const num1 of arr){
+        const num2 = sum - num1
+        if(nums[num2]){
+            pairs.push([num1, num2])
+        }
+    }
+}
