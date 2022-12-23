e65dd3dee39029c558979ba17de16fc395d48a01
Justin Moy <moy.justin94@gmail.com>
2022-12-23 16:57:32 -0600

    leetcode practice

--- 7kyu/leetcode/day7.js
+++ 7kyu/leetcode/day7.js
@@ -31,3 +31,11 @@ console.log(fib(2),1)
 console.log(fib(4),3)
 
 //will there be any neg nums, spec chars, spaces, decimals, strings, or empty arrays
+
+function fib(n){
+    let arr = [0,1]
+    for(let i = 2; i <= n; i++){
+        arr[i] = arr[i - 1] + arr[i - 2]
+    }
+    return arr[n]
+}
