7a33d4d73a5a4f99cebcab2a7af481fe0e2cf7d4
Justin Moy <moy.justin94@gmail.com>
2022-12-24 14:01:49 -0600

    leetcode practice

--- 7kyu/leetcode/day7.js
+++ 7kyu/leetcode/day7.js
@@ -39,3 +39,11 @@ function fib(n){
     }
     return arr[n]
 }
+
+function fib(n){
+    let arr = [0,1]
+    for(let i = 2; i <= n; i++){
+        arr[i] = arr[i - 1] + arr[i - 2]
+    }
+    return arr[n]
+}
