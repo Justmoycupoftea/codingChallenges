a39799759e6101229c755b68599e5f7b5d756f0b
Justin Moy <moy.justin94@gmail.com>
2023-01-11 20:38:19 -0600

    leetcode problem

--- 7kyu/leetcode/day1.js
+++ 7kyu/leetcode/day1.js
@@ -77,3 +77,20 @@ function findJewels(jewels, stones) {
     }
     return count
 };
+
+
+function findJewels(jewels, stones) {
+    const map = {}
+    let count = 0
+
+    for(const jewel of jewels){
+        map[jewel] = true
+    }
+
+    for(const stone of stones){
+        if( map[stone] ){
+            count++
+        }
+    }
+    return count
+};
