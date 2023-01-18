b376c9e1bb7ea027e8d0b416fb835ca21f8e3af4
Justin Moy <moy.justin94@gmail.com>
2023-01-17 20:27:07 -0600

    leetcode practice

--- 7kyu/leetcode/day1.js
+++ 7kyu/leetcode/day1.js
@@ -62,34 +62,3 @@ function findJewels(jewels, stones) {
 
 //more practice here
 
-function findJewel(j, s) {
-    const map = {}
-    let count = 0
-
-    for(const je of j){
-        map[je] = true
-    }
-
-    for(const st of s){
-        if( map[stone] ){
-            count++
-        }
-    }
-    return count
-}
-
-function findJewel(j, s) {
-    const map = {}
-    let count = 0
-
-    for(const je of j){
-        map[je] = true
-    }
-
-    for(const st of s){
-        if( map[stone] ){
-            count++
-        }
-    }
-    return count
-}
--- 7kyu/leetcode/day2.js
+++ 7kyu/leetcode/day2.js
@@ -29,3 +29,23 @@ function maxCharacter(str){
 }
 
 console.log(maxCharacter("Hello World!"), 'l')
+
+function maxCharacter(str){
+    let charMap = {},
+    count = 0,
+    maxChar = null
+
+    //create object
+    for(const char of str){
+        charMap[char] = charMap[char] + 1 || 1
+        
+    }
+    //looping through
+    for(const char in charMap){
+        if(charMap[char] > count){
+            count = charMap[char]
+            maxChar = char
+        }
+    }
+    return maxChar
+}
