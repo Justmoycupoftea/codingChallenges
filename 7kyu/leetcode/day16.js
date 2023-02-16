7ae44b71587fd0f0d7f10b7d4c0dc51aed22f705
Justin Moy <moy.justin94@gmail.com>
2023-02-16 16:12:33 -0600

    leetcode problem

--- 7kyu/leetcode/day16.js
+++ 7kyu/leetcode/day16.js
@@ -60,3 +60,32 @@ class Queue {
 
   //linked list problem
   //data points to the next bit of data continiously
+
+  //queue practice
+  class Queue {
+    constructor() {
+      this.storage = {}
+      this.head = 0
+      this.tail = 0
+    }
+    enqueue(element) {
+      this.storage[this.tail] = element
+      this.tail++
+    }
+    dequeue() {
+      let removed = this.storage[this.head]
+      delete this.storage[this.head]
+      this.head++
+      return removed
+    }
+  }
+  
+  const queue = new Queue()
+  
+  queue.enqueue('apple')
+  queue.enqueue('banana')
+  queue.enqueue('blueberry')
+  
+  queue.dequeue('apple')
+  
+  queue.enqueue('strawberry')
