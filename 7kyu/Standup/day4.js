0ddc38d404b215caaaf052f17178106fc1523dda
Justin Moy <moy.justin94@gmail.com>
2023-02-25 11:51:01 -0600

    codewar practice

--- 7kyu/Standup/day4.js
+++ 7kyu/Standup/day4.js
@@ -53,3 +53,8 @@ function getAvg(marks){
 
   return Math.floor(sum / marks.length)
 }
+function getAvg(marks){
+  let sum = marks.reduce((a,c) => a + c)
+
+  return Math.floor(sum / marks.length)
+}
