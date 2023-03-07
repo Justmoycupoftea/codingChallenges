e62be2ed6213314ff66ef56d9a91dfed66addbc3
Justin Moy <moy.justin94@gmail.com>
2023-03-06 20:47:32 -0600

    more middle node practice

--- 7kyu/leetcode/day18.js
+++ 7kyu/leetcode/day18.js
@@ -40,3 +40,13 @@ var middleNode = function(head) {
     }
     return slow
 }
+
+//arrows are changing moving tail to new head, arrows flip
+var middleNode = function(head) {
+    slow = fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow
+}
