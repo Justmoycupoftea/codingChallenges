6559a18dace6746642f7eb02aba174d5d59b4d78
Justin Moy <moy.justin94@gmail.com>
2023-03-07 20:11:34 -0600

    linked list practice

--- 7kyu/leetcode/day19.js
+++ 7kyu/leetcode/day19.js
@@ -74,3 +74,33 @@ class LinkedList {
     return prev
   }
 }
+
+class LinkedList {
+  constructor () {
+    this.head = this.tail = null
+  }
+
+  append(value) {
+
+    if (!this.tail) {
+      this.head = this.tail = new Node(value)
+    }
+ 
+    else {
+      let oldTail = this.tail
+      this.tail = new Node(value)
+      oldTail.next = this.tail
+    }
+  }
+  reverseList(){
+     let current = this.head
+     let prev = null
+     while (current) {
+        const temp = current.next
+        current.next = prev
+        prev = current
+        current = temp
+    }
+    return prev
+  }
+}
