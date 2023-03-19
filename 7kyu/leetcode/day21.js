5dceed5c52010ed6fab94c6657ddd1cd158b6aeb
Justin Moy <moy.justin94@gmail.com>
2023-03-19 11:30:57 -0500

    binary tree

--- 7kyu/leetcode/day21.js
+++ 7kyu/leetcode/day21.js
@@ -42,3 +42,18 @@ function binarySearch(nums, target) {
 }
 console.log(binarySearch([-1,0,3,5,9,12],9),"4")
 console.log(binarySearch([-1,0,3,5,9,12],2),"-1")
+
+function binarySearch(nums, target) {
+    let left = 0, right = nums.length-1
+    while (left < right) {
+        let mid = Math.ceil((left + right) / 2)
+        //let mid = left + Math.floor((right-left+1)/2)
+        if (target < nums[mid]) {
+            right = mid - 1
+        } else {
+            left = mid + 1
+            //left = mid
+        }
+    }
+    return nums[left] === target ? left : -1
+}
