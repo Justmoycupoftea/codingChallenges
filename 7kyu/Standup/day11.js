a4cb338f36aa9454c11c59baec059c7fa46b2541
Justin Moy <moy.justin94@gmail.com>
2023-04-18 14:57:05 -0500

    codewar

--- 7kyu/Standup/day11.js
+++ 7kyu/Standup/day11.js
@@ -45,3 +45,14 @@ function last(x) {
 //will it alays be words, will it be empty, any spaces or special characters
 //return sorted array of words and the last letter of each word
 
+//sort string in order by the last letter of each word, grab last letter
+
+function lasty(x) {
+//use a sort, have to split and sort array
+//sort is lexigraphical, sorts by letters already
+//grab last letter and can sort it
+return x.split('').sort((a,b)=> a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1))
+//charcodeat treats the string as an index, charcode starts counting at 0
+//comparing letters to each other with sort
+
+}
