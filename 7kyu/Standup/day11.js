43729aacea1a0e615d022b8316d3af02318a9905
Justin Moy <moy.justin94@gmail.com>
2023-04-18 11:28:40 -0500

    codewar problem

--- 7kyu/Standup/day11.js
+++ 7kyu/Standup/day11.js
@@ -35,3 +35,13 @@ function last(x) {
 function last(x) {
     return x.split('').sort((a,b)=> a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1))
 }
+
+// Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+// If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+// All inputs will be valid.
+
+//will it alays be words, will it be empty, any spaces or special characters
+//return sorted array of words and the last letter of each word
+
