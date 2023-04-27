a8b8cf0b09f4fe674a7de2c62914f41e3761ad0f
Justin Moy <moy.justin94@gmail.com>
2023-04-27 18:31:13 -0500

    codewar problem

--- 7kyu/Standup/day15.js
+++ 7kyu/Standup/day15.js
@@ -57,3 +57,15 @@ function sortArray(array) {
     
    return array;
   }
+
+// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+// Examples
+// [7, 1]  =>  [1, 7]
+// [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+//are we getting all nums, no spaces, no funny bizz, no strings
+//return even numbers in same order, sort odd nums
+//filter all the uneven nums, sort the uneven nums
+//return the new mapped array if num is even just return in same spot, and add all uneven nums into new array
