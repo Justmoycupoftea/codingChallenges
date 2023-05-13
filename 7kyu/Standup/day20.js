dcf10afa16b441fa3b50283eb7d92b06df17e24e
Justin Moy <moy.justin94@gmail.com>
2023-05-13 12:21:20 -0500

    codewar

--- 7kyu/Standup/day20.js
+++ 7kyu/Standup/day20.js
@@ -72,3 +72,17 @@ function fizzbuzz(nums){
         }
     }
 }
+
+function fizzbuzz(nums){
+    for (let i = 1; i <= nums; i++){
+        if(i % 2 === 0 && i % 3 === 0){
+            return 'fizzbuzz'
+        }else if(i % 2 === 0){
+            return 'fizz'
+        }else if(i % 3 === 0){
+            return 'buzz'
+        }else{
+            return i
+        }
+    }
+}
