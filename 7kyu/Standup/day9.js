d83855c740dafe6f33c5c179bf3fd0cae01d91d7
Justin Moy <moy.justin94@gmail.com>
2023-05-28 10:09:21 -0500

    codewar

--- 7kyu/Standup/day9.js
+++ 7kyu/Standup/day9.js
@@ -67,3 +67,7 @@ function dupes(word){
 function dupes(word){
   return word.toLowerCase().split('').map( function(a,i,w) {return w.indexOf(a) === w.lastIndexOf(a) ? '(' : ')'}).join('')
 }
+
+function dupes(word){
+  return word.toLowerCase().split('').map( function(a,i,w) {return w.indexOf(a) === w.lastIndexOf(a) ? '(' : ')'}).join('')
+}
