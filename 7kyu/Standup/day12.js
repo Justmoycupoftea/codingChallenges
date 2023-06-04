bb67fb3bf3cd04e5779d61c85af255a2627b00d1
Justin Moy <moy.justin94@gmail.com>
2023-06-04 11:07:24 -0500

    codewar

--- 7kyu/Standup/day12.js
+++ 7kyu/Standup/day12.js
@@ -80,3 +80,29 @@ function missNum(arr, mixArr){
         return sum - mixSum
     }
 }
+
+// An ordered sequence of numbers from 1 to N is given. One number might have deleted from it, then the remaining numbers were mixed. Find the number that was deleted.
+
+// Example:
+
+// The starting array sequence is [1,2,3,4,5,6,7,8,9]
+// The mixed array with one deleted number is [3,2,4,6,7,8,1,9]
+// Your function should return the int 5.
+// If no number was deleted from the starting array, your function should return the int 0.
+
+// Note: N may be 1 or less (in the latter case, the first array will be []).
+
+//using filter and indexOf is using a loop inside a loop making it linear so slow
+
+//anki problem memorize this
+function missNum(arr, mixArr) {
+    //constant check
+    if (arr.length === mixArr.length){
+        return 0
+    }else{
+        let arrSum = arr.reduce((a,b)=> a + b, 0)
+        let mixArrSum = mixArr.reduce((a,b)=> a + b, 0)
+        return arrSum - mixArrSum
+        //linear loops, non nested so theyre not as slow
+    }
+}
