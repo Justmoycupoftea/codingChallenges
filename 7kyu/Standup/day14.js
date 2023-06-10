736fc927ec0c6c4131cfc75f8d04faadfec7a0f5
Justin Moy <moy.justin94@gmail.com>
2023-06-09 21:49:35 -0500

    codewar

--- 7kyu/Standup/day14.js
+++ 7kyu/Standup/day14.js
@@ -37,3 +37,10 @@ function giveme(three){
     //index of the middle of three nums would be 1  
     //sort array and find index of num
 }
+
+function gimmethree(three){
+    return three.indexOf([...three].sort((a,b)=> a - b[1]))
+    //index of the middle of three nums would be 1  
+    //sort array and find index of num
+
+}
