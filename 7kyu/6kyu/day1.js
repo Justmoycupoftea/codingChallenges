f62d8ea9e6a55b8cb1f40ef4284e6458671b9ea9
Justin Moy <moy.justin94@gmail.com>
2023-07-10 10:43:24 -0500

    codewar

--- 7kyu/6kyu/day1.js
+++ 7kyu/6kyu/day1.js
@@ -91,3 +91,18 @@ function sumM(nums){
     return emptySum
 }
 
+function sumNums(num){
+    if(num < 0){
+        return 0;
+    }
+
+    let sum = 0
+
+    for(let i = 3; i < num; i++){
+        if(i % 3 === 0 || i % 5 === 0){
+            sum += i
+        }
+    }
+    return sum
+}
+
