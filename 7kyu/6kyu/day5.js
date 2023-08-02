b786d6db5bd2b55bc76c3a855c48d1c3efa430aa
Justin Moy <moy.justin94@gmail.com>
2023-08-02 15:39:00 -0500

    working on recursion

--- 7kyu/6kyu/day5.js
+++ 7kyu/6kyu/day5.js
@@ -7,3 +7,29 @@
 //    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
 // 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
 // 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+function digitalRoot(n) {
+    // Base case: if n is a single-digit number, return n
+    if (n < 10) {
+      return n;
+    }
+  
+    let sum = 0;
+  
+    // Calculate the sum of digits
+    while (n > 0) {
+      sum += n % 10;
+      n = Math.floor(n / 10);
+    }
+  
+    // Recursively calculate digital root of the sum
+    return digitalRoot(sum);
+  }
+  
+  // Example usage
+  const number = 9875;
+  const result = digitalRoot(number);
+  console.log(`The digital root of ${number} is ${result}`);
+
+//   Replace the number variable with the non-negative integer you want to find the digital root of. The digitalRoot function takes care of calculating the digital root using recursion and returns the final single-digit result.
+  
