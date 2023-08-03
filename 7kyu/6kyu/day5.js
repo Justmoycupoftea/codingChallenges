585e2f53fc8021590f22799a0d543af36a4cc66a
Justin Moy <moy.justin94@gmail.com>
2023-08-03 14:48:23 -0500

    recursion prac

--- 7kyu/6kyu/day5.js
+++ 7kyu/6kyu/day5.js
@@ -50,3 +50,17 @@ function digitalRoot(n) {
     // Recursively calculate digital root of the sum
     return digitalRoot(sum);
   }
+function digRoot(n){
+    if (n < 10){
+        return n;
+    }
+
+    let sum = 0;
+
+    while (n > 0){
+        sum += n % 10;
+        n = Math.floor(n / 10);
+    
+    }
+    return digRoot(sum)
+}
