08dcf48f799804f61a9fb7d15652bc66a1c84b17
Justin Moy <moy.justin94@gmail.com>
2023-08-23 20:41:49 -0500

    codewar

--- 7kyu/6kyu/day5.js
+++ 7kyu/6kyu/day5.js
@@ -326,3 +326,20 @@ function digRoot(n){
 }
 
 
+function digRoot(n){
+    if (n < 10){
+        return n;
+    }
+
+    let sum = 0;
+
+    while (n > 0){
+        sum += n % 10;
+        n = Math.floor(n / 10);
+    
+    }
+    return digRoot(sum)
+}
+
+
+
